97b62eabc1bfce908dbaa8e8095ef3fff512788c
Westley Winks <westleywinks@protonmail.com>
2023-08-11 19:46:38 +0100

    added content

--- scripts/obsidian-to-hugo.py
+++ scripts/obsidian-to-hugo.py
@@ -1,6 +1,5 @@
 from obsidian_to_hugo import ObsidianToHugo
 from pathlib import Path
-import frontmatter as fm
 
 def included_in_hugo(file_contents: str, file_path: str) -> bool:
     # do something with the file path and contents
@@ -14,7 +13,7 @@ obsidian_to_hugo = ObsidianToHugo(
     hugo_content_dir=Path.cwd() / "content",
     filters=[included_in_hugo],
     copy_exclusions = ["books", ".obsidian", "peace-corps", "journal", "tattoos"],
-    direct_copies = [Path("02_Areas/peace-corps"), Path("03_Resources/books")]
+    direct_copies = [Path("2 Areas/peace-corps"), Path("3 Resources/books")]
 )
 
 obsidian_to_hugo.run()
